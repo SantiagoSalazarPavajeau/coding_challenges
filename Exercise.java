3343180a93aac5483a0280d4fd67bf6b8ded1ae5
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2020-06-15 18:13:48 +0200

    add java countingValleys method

---
+++ Exercise.java
@@ -0,0 +1,30 @@
+
+
+public class Exercise{
+
+    static void countingValleys(int steps, String path){
+        char[] hike = path.toCharArray();
+        int elevation = 0;
+        int valleys = 0;
+        char up = 'U';
+        char down = 'D';
+
+        for(int i = 0; i < steps; i++){
+            if(hike[i] == up){
+                elevation += 1;
+            }else if(hike[i]== down){
+                if(elevation == 0){
+                    valleys += 1;
+                }
+                elevation -= 1;
+            }
+        }
+        System.out.println(valleys);
+    }
+
+    public static void main (String[] args){
+        countingValleys(8, "DDUUDDUU");
+    }
+
+
+}
