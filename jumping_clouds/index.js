827f7d780f81264171458879584136acf6812b53
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2020-06-16 10:44:59 +0200

    add jumping clouds exercise

---
+++ jumping_clouds/index.js
@@ -0,0 +1,12 @@
+let jumpingClouds = (clouds) => {
+    let jumps = 0
+    for(const i =0; i < clouds.length; i++){
+        if(clouds[i] === 0){
+            jumps+=1
+        }else if (clouds[i] === 1){
+            jumps+=0
+        }
+    }
+}
+
+jumpingClouds([0,1,0,0,0,1,0])
