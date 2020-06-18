57342cbd18fae3f11e96c5b79b83d3205ecc8c6a
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2020-06-18 12:03:59 +0200

    working 2d array

---
+++ 2d-array/index.js
@@ -0,0 +1,30 @@
+twodarray = [
+                [1, 1, 1, 0, 0, 0],     
+                [0, 1, 0, 0, 0, 0],    
+                [1, 1, 1, 0, 0, 0],     
+                [0, 0, 2, 4, 4, 0],     
+                [0, 0, 0, 2, 0, 0],    
+                [0, 0, 1, 2, 4, 0]
+            ]
+
+twoDimensionalArrayHourglass(twodarray)
+
+function twoDimensionalArrayHourglass(array){
+    let hourGlassSums = []
+    for(let row = 0; row <= 3; row++){
+        for(let column = 0; column <= 3; column++){
+            let hourGlassSum = 0
+            hourGlassSum += array[row][column];
+            hourGlassSum += array[row][column + 1];
+            hourGlassSum += array[row][column + 2];
+            hourGlassSum += array[row + 1][column + 1];
+            hourGlassSum += array[row + 2][column]; 
+            hourGlassSum += array[row + 2][column + 1];
+            hourGlassSum += array[row + 2][column + 2];
+            hourGlassSums.push(hourGlassSum)
+        }
+    }
+    console.log(Math.max(...hourGlassSums))
+    console.log(hourGlassSums)
+
+}
--- repeated_strings/Exercise.java
+++ repeated_strings/Exercise.java
@@ -2,7 +2,7 @@ package repeated_strings;
 
 public class Exercise{
 
-    static void repeatedStrings(String string, long number){
+    static void repeatedStrings(String string, int number){
         int stringLength = string.length();
         int countOfAs = 0; // can use this for the end
         char[] stringArray = string.toCharArray(); // can use this to loop over string
@@ -11,7 +11,7 @@ public class Exercise{
                 countOfAs++;
             }
         }
-        long wholeRepeats = number / stringLength;
+        int wholeRepeats = number / stringLength;
         int remainingAs = 0;
         String remainigString = string.substring(0,number % stringLength);
         char[] remainingStringArray = remainigString.toCharArray();
@@ -28,3 +28,25 @@ public class Exercise{
         
     }
 }
+
+// char[] chrIn = s.toCharArray(); 
+// int i = 0; 
+// long max = 0; 
+// long quo = n / chrIn.length; 
+// long rem = n % chrIn.length; 
+// while (i < chrIn.length) { 
+//     if (chrIn[i] == 'a') { 
+//         max++; 
+//     } 
+//     i++; 
+// } 
+// i = 0; 
+// max = max * quo; 
+// while (i < rem) {
+//      if (chrIn[i] == 'a') { 
+//          max++; 
+//         } 
+//         i++;
+
+//     }
+//     return max;
