12426cb6eee2d1b7404ff00a9c5bf25e84ad9ace
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2020-06-18 13:38:44 +0200

    add array management methods

--- 2d-array/Exercise.java
+++ 2d-array/Exercise.java
@@ -1,11 +1,13 @@
-
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 public class Exercise{
 
     static void nestedArrayHourglass(int[][] array){
-        int[] hourGlassSums = new int[22];
+        int[] hourGlassSums = new int[16];
+        int i = 0;
         for(int row_index = 0; row_index <= 3; row_index++){
-            for(int column_index = 0; row_index <= 3; column_index++){
+            for(int column_index = 0; column_index <= 3; column_index++){ 
                 int hourGlassSum = 0;
                 hourGlassSum += array[row_index][column_index];
                 hourGlassSum += array[row_index][column_index + 1];
@@ -14,11 +16,14 @@ public class Exercise{
                 hourGlassSum += array[row_index + 2][column_index]; 
                 hourGlassSum += array[row_index + 2][column_index + 1];
                 hourGlassSum += array[row_index + 2][column_index + 2];
-                int i = 0;
                 hourGlassSums[i] = hourGlassSum;
+                i++;
             }
         }
-        System.out.println(hourGlassSums);
+
+        // System.out.println(Arrays.toString(hourGlassSums));
+        System.out.println(Arrays.stream(hourGlassSums).max().getAsInt());
+        
     }
     public static void main(String[] args){
         int[][] twoDArray = {
