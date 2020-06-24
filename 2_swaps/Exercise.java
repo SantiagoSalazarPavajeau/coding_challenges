c9788995127932d613785b2ed9ed5cc738282a8f
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2020-06-24 11:32:27 +0200

    add console.log tracking and explicit variable names

--- 2_swaps/Exercise.java
+++ 2_swaps/Exercise.java
@@ -1,8 +1,18 @@
 public class Exercise{
 
-    static void twoSwapSort(int[] unsortedArray){
+    static void int twoSwapSort(int[] unsortedArray){
         int swaps = 0;
-        boolean[] virtualVisitingArray=  new boolean[unsortedArray.length];
+        boolean[] inspectionArray=  new boolean[unsortedArray.length];
+        for(int outerIndex = 0; outerIndex < unsortedArray.length; outerIndex++){ //loop through unsortedArray
+            int indexToValue = outerIndex // this is what the element is supposed to look like in correct array
+            int cycle = 0
+            while(!inpsectionArray[indexToValue]){ // elements that have not been inspected are false
+                inspectionArray[indexToValue] = true; 
+                indexToValue = unsortedArray[indexToValue] -1
+
+            }
+
+        }
     }
 
     public static void main(String[] args){
--- 2_swaps/index.js
+++ 2_swaps/index.js
@@ -3,23 +3,24 @@
 
 const swapTwoToSort = (unsortedArray)=>{
     let swaps = 0
-    let virtualSortedArray = []
+    let inspectionArray = []
     for (let outerIndex = 0; outerIndex < unsortedArray.length; outerIndex++) {
-        console.log("i \t ->"+outerIndex)
+        console.log("Unsorted Index is: "+outerIndex)
         let innerIndex = outerIndex, cycle = 0;
-        console.log("j \t ->" + innerIndex)
-        while (!virtualSortedArray[innerIndex]) {
-            console.log("We are inside while loop ")
-            virtualSortedArray[innerIndex] = true; // create the current element on the virtual array
-            console.log(`Just set the: ${innerIndex} element to ${virtualSortedArray[innerIndex]}`)
+        console.log("Index to value is: " + innerIndex)
+        while (!inspectionArray[innerIndex]) {
+            console.log("\tWe are inside while loop... ")
+            inspectionArray[innerIndex] = true; // create the current element on the virtual array
+            console.log(`\tWe just inspected the: [${innerIndex}] element`)
+            console.log(`\tThe current element is: ${unsortedArray[innerIndex]}`)
             innerIndex = unsortedArray[innerIndex] - 1; // change the j or inner index to be the the j element on the original array
-            console.log("j \t ->" + innerIndex)
-            console.log(virtualSortedArray)
+            console.log(`\tWe just set the index to value to:  ${unsortedArray[innerIndex] - 1}`)
             cycle++;
+            console.log(`\tWe just counted ${cycle} cycles.`)
         }
         if (cycle != 0)
             swaps += cycle - 1;
-        console.log("Total swaps: \t ->" + swaps)
+        console.log("\t\tTotal swaps: ->" + swaps)
     }
     return swaps;
 }
