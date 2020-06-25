6c3727b2163ef16e9084f2a597c41c775faaabc6
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2020-06-25 16:49:01 +0200

    add big O notation notes

---
+++ big_O_notation/Exercise.java
@@ -0,0 +1,32 @@
+public class Exercise{
+
+    // O(1)
+    static void printFirstItem(int[] items) {
+        System.out.println(items[0]);
+    }
+
+    // O(n)
+    static void printAllItems(int[] items) {
+        for (int item : items) {
+            System.out.println(item);
+        }
+    }
+
+    // O(n^2)
+    static void printAllPossibleOrderedPairs(int[] items) {
+        for (int firstItem : items) {
+            for (int secondItem : items) {
+                System.out.println(firstItem + ", " + secondItem); // nested loops
+            }
+        }
+    }
+
+
+    public static void main(String[] args){
+        printFirstItem(items);
+    }
+
+
+
+
+}
