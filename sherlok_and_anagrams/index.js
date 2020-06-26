64aab33b3da98b1d0da919c1151de61d22f27d28
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2020-06-26 12:18:34 +0200

    add console.log tracking

--- sherlok_and_anagrams/index.js
+++ sherlok_and_anagrams/index.js
@@ -1,9 +1,10 @@
 const createSubstrings = (string) => {
-    let outerIndex, innerIndex;
     let substrings = [];
-    for(outerIndex =0;outerIndex<string.length;outerIndex++){
-        for(innerIndex = outerIndex; innerIndex<string.length +1; innerIndex++){
+    for(let outerIndex =0;outerIndex<string.length;outerIndex++){
+        for(let innerIndex = outerIndex+1; innerIndex<string.length +1; innerIndex++){
+            console.log(string.slice(outerIndex,innerIndex))
             substrings.push(string.slice(outerIndex,innerIndex))
+            console.log(`The slice range starts ${outerIndex} and ends in ${innerIndex} loop (does not include last value)`)
         }
     }
     console.log(substrings)
