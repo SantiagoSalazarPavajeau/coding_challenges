4d9e34860282ad70ccc06bd282962ab7205fd2b2
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2020-10-23 15:48:56 -0400

    add almost fully  working longest palindrome

--- palstring/index.js
+++ palstring/index.js
@@ -1,24 +1,68 @@
 //thequickbrownfoxxofnworbquickthe  ->  brownfoxxofnworb, The length is: 16
 
+const expandedString = (string) => {
+    let stringArray = []
+    for(let i = 0;  i < string.length; i++){
+        stringArray.push(string[i])
+        stringArray.push("")
+    }
+    stringArray.pop()
+    return stringArray
+}
 
 const maxPalindromeSubString = (string) => {
+    if(string.length <= 1){
+        console.log(string[0])
+        return string[0]
+    }
+    let stringWithSpaces = expandedString(string)
     let result = [];
-    for(let i = 0;  i < string.length; i++){
-            let lowI = i
-            let highI = string.length - (i+1)
-            if(highI > lowI){
-                if(string[lowI] === string[highI] && string[lowI+1] === string[highI-1] ){
-                    
-                    result.push(string[i])
+    let lowI; //initialize here to be in scope to return result
+    let highI;
+    for(let i = 0;  i < stringWithSpaces.length; i++){
+            let palindrome = []
+            // inner loop to expand from each character (spaces and letters)
+            for(let j = 0;  j < stringWithSpaces.length; j++){ // loop outwards on every center
+                // console.log(`-${stringWithSpaces[i]}, ${i}`)
+                // console.log("        ")
+                if(!!stringWithSpaces[i] && stringWithSpaces[i-1] === stringWithSpaces[i+1]){ // if adjacent characters are the same expand on that center
+                    // console.log(`-${stringWithSpaces[i]}, ${i}`)
+                    // expand
+                    palindrome.push[stringWithSpaces[i]] // add center
+                    if( !!stringWithSpaces[i-j] && stringWithSpaces[i-j] === stringWithSpaces[i+j]){ // keep expanding if longer palindrome found
+                        console.log(`-${stringWithSpaces[i-j]}`)
+                        console.log(`-${stringWithSpaces[i+j]}`)
+                        palindrome.unshift(stringWithSpaces[i-j])
+                        palindrome.push(stringWithSpaces[i+j])
+                        console.log(palindrome)                
 
+                    }
                 }
             }
+            result.push(palindrome)
     }
-    console.log(`${result.toString()}, ${result.length}`)  
-    return `${result.toString()}, ${result.length}`
-   }
+    // console.log(result)
+    result = result.filter((pal) => pal.length !== 0) // only return the arrays that are not empty
+    let cleanedResult = [];
+    for(let pal of result){ // clean up empty strings and undefined values
+        cleanedResult.push(pal.filter((char) => !!char))
+    }
+    // console.log(cleanedResult)
 
-maxPalindromeSubString('thequickbrownfoxxofnworbquickthe')
+    
+    if(cleanedResult.length === 0){
+        // console.log(string[0])
+        return string[0];
+    } else{
+        let correctIndex = 0;
+        correctIndex = cleanedResult.reduce((ans, pal, i, array) => array[ans].length > pal.length ? ans : i, 0) // Find array with longest length (this reduce returns the index)
+        console.log(`${cleanedResult[correctIndex].join('')}, ${cleanedResult[correctIndex].length}`)
+        return `${cleanedResult[correctIndex].join('')}, ${cleanedResult[correctIndex].length}`
+    }
+
+}
+
+maxPalindromeSubString('babad')
 
 // *Program to print unique elements of an string present on odd position, along with the count of those elements in the entire string
 // i.e abbloseckk - a:1,b:2,o:1,e:1,k:2
@@ -50,7 +94,6 @@ const uniqueElements = (string) => {
 
 const secondLargestNumber = (array) => {
     array.sort((a,b)=>b-a)
-    console.log(array[1])
     return array[1]
 }
 
