4556d64cb9ef0a425113d8059d08a5f3e94110e0
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2020-11-02 11:03:06 -0500

    binary search description

--- binary_search/index.js
+++ binary_search/index.js
@@ -1,3 +1,7 @@
+// find a character on a string or a number on an array 
+// sort the numbers/strings and implement the phone book method
+// split the data and see if our value is higher or lower and then split again..
+
 function binarySearch(array, value){
     
     let highIndex = array.length -1;
