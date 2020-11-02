0a59fc3a41ae0fdaf10f453b97c44ca8e37f6f6d
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2020-11-02 10:06:40 -0500

    update twosum

--- two_sums/index.js
+++ two_sums/index.js
@@ -1,14 +1,21 @@
 // https://leetcode.com/problems/two-sum/
-const twoSum = (array, target) => {
-    for(let i = 0; i<array.length; i++){
-        for(let j = i+1; j<array.length; j++){
-            if(array[i] + array [j] === target){
-                console.log([i, j])
-                return [i, j];
-            }
-        }
-    }
+// const twoSum = (array, target) => {
+//     for(let i = 0; i<array.length; i++){
+//         for(let j = i+1; j<array.length; j++){
+//             if(array[i] + array [j] === target){
+//                 console.log([i, j])
+//                 return [i, j];
+//             }
+//         }
+//     }
 
-}
+// }
+
+// find the indexes of two numbers that their sum is the second argument passed to the twoSum method
 
 twoSum([2,5,5,11], 10)
+//      0 1 2 3
+// correct answer [1,2]
+const twoSum = () => {
+
+}
