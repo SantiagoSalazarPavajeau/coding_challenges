37bf0546e37849a521bdce7db585f2c1c637f459
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2020-11-10 14:35:31 -0500

    add binary tree object representation

--- binary_tree_depth/index.js
+++ binary_tree_depth/index.js
@@ -1,4 +1,26 @@
-// binary trees have the methods .val .right and .left
+// binary trees can be represented as nested objects with the keys: .val .right and .left
+
+const ourBinaryTree = {
+    val: 3,
+    right: {
+        val: 20,
+        right: { 
+            val: 7, 
+            right: null, 
+            left: null
+        },
+        left: { 
+            val: 15, 
+            right: null, 
+            left: null
+        }
+    },
+    left: {
+        val: 9,
+        right: null,
+        left: null
+    }
+}
 
 //          3
 //         / \
@@ -10,21 +32,25 @@
 // root.right is 20
 // root.left is 9
 
+// at node 20 node.val is 3
+// node.right is 7
+// node.left is 15
+
 // binary trees are searched by recursion(the function calling itself)
 
-// const treeDepth = (root) => {
-//     if(root === null) return 0 //
-//     return Math.max(treeDepth(root.right), treeDepth(root.left))
-// }
+
 
 const getMaxDepth = (root) => {
     if(root === null){ //if we reached an empty leaf return the count so far
-        return 0
+        return 0 // this stops the count
     }
      // looping by recursion 
      // with two properties right and left
-    return Math.max( maxDepth(root.left) +1 , maxDepth(root.right) +1 )
+    console.log(Math.max( getMaxDepth(root.left) + 1 , getMaxDepth(root.right) + 1 ))
+    return Math.max( getMaxDepth(root.left) + 1 , getMaxDepth(root.right) + 1 ) // 
 }
 
 //     1
 //  2
+
+getMaxDepth(ourBinaryTree)
