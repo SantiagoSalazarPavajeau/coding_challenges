e6ebeca90ca3d8ab6c0e02c1a32fdf1282d39c33
Santiago Salazar Pavajeau <santiago.s.pavajeau@gmail.com>
2020-12-11 15:10:02 -0500

    add js with hashmap

--- subarray-sum-w-target/index.js
+++ subarray-sum-w-target/index.js
@@ -3,33 +3,42 @@ var subarraySum = function(nums, k) {
     let sum = 0
     
     let dataObject = {}
-    
-    for(let i = 0; i <= nums.length ; i++){
+    console.log(nums)
+    for(let i = 0; i <= nums.length-1; i++){
 
         sum += nums[i]
-        if(sum === k)
+        console.log('sum', sum)
+        console.log('nums[i]',nums[i])
+        console.log('i', i)
+        console.log('k', k)
+        console.log('     ')
+
+
+        if(sum === k){
             count++
-        
-        if(dataObject[sum - k]){
+        }
+
+        if(dataObject[sum - k]){ // 
             count += dataObject[sum -k]
         }
 
         if(dataObject[sum]){
             dataObject[sum]++
         }else{
-            dataObject[sum] = 1
+            dataObject[sum] = 1 // last loop sum = NaN
         }
 
         // dataObject[sum] = sum in dataObject ? dataObject[sum] + 1 : 1
+        console.log(dataObject)
+
 
     }
 
-    console.log(dataObject)
     
     return count;
 };
 
-console.log(subarraySum([1,1,1], 1))
+console.log(subarraySum([1,1,1], 2))
 
 
 // [{0=1, 1=1}]
