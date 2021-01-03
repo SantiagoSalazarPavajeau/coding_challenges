e3373cbb030a338bc6ff3cced88ab1a7b2965da7
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2021-01-03 16:30:19 -0300

    add regex comments

--- valid_palindrome/index.js
+++ valid_palindrome/index.js
@@ -3,7 +3,12 @@
 
 
 const validPalindrome = (s) => {
-    let clean = s.toLowerCase().replace(/[^a-z\d]/g, '')
+    let clean = s.toLowerCase().replace(/[^a-z\d]/g, '')  
+    // regex .replace method 
+    // g flag is for global matches so wont stop after the first match is found
+    // a-z return only  characters
+    // \d return only digits
+    //  '' all non characters and non digits replace with an empty string
     
     let arrayS = clean.split('')
 
