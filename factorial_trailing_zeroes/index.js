addfe4578bef494b78081287a4fd00f69143d900
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2021-01-15 12:25:24 +0100

    add js with log complexity

--- factorial_trailing_zeroes/index.js
+++ factorial_trailing_zeroes/index.js
@@ -30,3 +30,21 @@ var trailingZeroes = function(n) { /// only works on small integers
     }
     return count 
 };
+
+
+
+let factorialTrailing0s = (n) => {
+    let count = 0; 
+  
+
+    for (let i = 5; n / i >= 1; i *= 5) { 
+        // loop to find how many 5s in prime factors 
+        // if our number(n) is greater than 1 when divided by five we count++
+        // 10/5 = 2 there are two prime factors 5 in 10
+        // 10/25 = 0.4 -> exit the loop
+        // so there are 2 trailing zeroes
+
+        count += Math.floor(n / i); 
+  }
+    return count;
+}
