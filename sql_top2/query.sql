4f286314328c8e506d9306b8a1d2cb17acbc16b5
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2021-01-18 17:39:58 +0100

    add sql ifnull, limit, offset

---
+++ sql_top2/query.sql
@@ -0,0 +1,11 @@
+--- get second highest salary
+
+SELECT
+    IFNULL(
+        (SELECT DISTINCT
+            Salary
+        FROM
+            Employee
+        ORDER BY Salary DESC
+        LIMIT 1 OFFSET 1), 
+        NULL) AS SecondHighestSalary
