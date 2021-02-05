440e567db78a3617b1ad06bfe81fe68b60f3f86d
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2021-02-05 13:20:31 +0100

    add pull from priorityqueue

--- heap/index.js
+++ heap/index.js
@@ -5,8 +5,12 @@
 // only rule is that children are smaller than parent
 
 class MinBinaryHeap{
-    constructor(heapArray){
+    constructor(){
         this.heapArray = []
+    }
+
+    getTop(){
+        return this.heapArray[0]
     }
 
     insertElement(newElement){
@@ -21,10 +25,61 @@ class MinBinaryHeap{
             if(newElementParent > newElement){
                 this.swap(newElementParentId, newElementId) // swap elements (found by id on this.heapArray)
                 newElementId = newElementParentId // swap ids in this while loop scope
+                // console.log(this.heapArray)
             }else{
                 break
             }
         }
+    }
+
+
+
+    getTopElementAndReorder(){ // pull out first priority item and set new next priority item
+        // first element is shot out 
+        // and last element becomes first element
+        // last element is not the smallest so we have to find the smallest to go on top of the heap
+        // visit leaves and compare value
+        //    - make sure leaves exist
+        //    - swap parent if leaf is smaller, but swap for smallest of two leaves
+        let topElement = this.heapArray[0]
+
+        if(this.heapArray.length < 1) return null
+
+        this.heapArray[0] = this.heapArray[this.heapArray.length - 1]
+        this.heapArray.pop()
+
+        let parentId = 0
+        let leftLeafId = (parentId * 2) + 1
+        let rightLeafId =  (parentId * 2) +2
+        let leftLeaf = this.heapArray[leftLeafId]
+        let rightLeaf = this.heapArray[rightLeafId]
+        let parent = this.heapArray[parentId]
+
+        while(!!leftLeaf && !!rightLeaf || !!leftLeaf){ //while both leaves exist or only the left leaf
+
+
+            let minLeaf = Math.min(leftLeaf, rightLeaf)
+            if( minLeaf < parent){ // if either one of the two leaves is less than parent swap
+
+                if(minLeaf === leftLeaf){
+                    this.swap(parentId, leftLeafId) // swap values
+                    parentId = leftLeafId // swap ids to move on down the heap
+                }
+
+                if(minLeaf === rightLeaf) {
+                    this.swap(parentId,rightLeafId)
+                    parentId = rightLeafId
+                }
+            } else break
+
+            leftLeafId = (parentId * 2) + 1
+            rightLeafId =  (parentId * 2 ) + 2
+
+            leftLeaf = this.heapArray[leftLeafId]
+            rightLeaf = this.heapArray[rightLeafId]
+            parent = this.heapArray[parentId]
+        }
+        return topElement
     }
 
     swap(parentId, leafId){
@@ -33,24 +88,55 @@ class MinBinaryHeap{
 }
 
 
-const priorities = [1,1,1,1,2,2,3,4,5,5,5]
-//                  0 1 2 3 4 5 6 7 8 9 10
+const priorities = [1,2,3,4,5]
+//                  0 1 2 3 4 
 const element = 0
 
 const heap = new MinBinaryHeap(priorities)
 
-heap.insertElement(1)
-heap.insertElement(1)
-heap.insertElement(1)
-heap.insertElement(1)
-heap.insertElement(2)
-heap.insertElement(2)
-heap.insertElement(3)
-heap.insertElement(4)
 heap.insertElement(5)
-heap.insertElement(5)
-heap.insertElement(5)
+console.log(heap.heapArray)
+heap.insertElement(1)
+console.log(heap.heapArray)
 
-heap.insertElement(element)
+heap.insertElement(4)
+console.log(heap.heapArray)
+
+heap.insertElement(2)
+console.log(heap.heapArray)
+
+heap.insertElement(3)
+console.log(heap.heapArray)
+
+heap.insertElement(1)
+console.log(heap.heapArray)
+
+
+
+
+// heap.insertElement(element)
 
 console.log(heap.heapArray)
+
+console.log(heap.getTopElementAndReorder())
+console.log(heap.heapArray)
+
+console.log(heap.getTopElementAndReorder())
+console.log(heap.heapArray)
+
+console.log(heap.getTopElementAndReorder())
+console.log(heap.heapArray)
+
+console.log(heap.getTopElementAndReorder())
+console.log(heap.heapArray)
+
+console.log(heap.getTopElementAndReorder())
+console.log(heap.heapArray)
+
+console.log(heap.getTopElementAndReorder())
+console.log(heap.heapArray)
+
+console.log(heap.getTopElementAndReorder())
+console.log(heap.heapArray)
+
+
