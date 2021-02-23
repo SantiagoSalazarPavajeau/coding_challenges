be9114147572c61b5c6740189bf6d779aecc6df8
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2021-02-23 17:18:01 +0100

    add js stack

--- valid_brackets/index.js
+++ valid_brackets/index.js
@@ -1,3 +1,66 @@
+// stack
+// push opening brackets to stack
+// pop only when bracket corresponds
+// if there are still elements its not valid
+
+
+const mB = (strings) => {
+    // console.log(string)
+    const dataBrackets = {
+        '}': '{',
+        ']': '[',
+        ')': '('
+    }
+
+    let result = []
+
+    for(let string of strings){
+
+        let stack = []
+        // console.log(string, 1)
+        let valid = true
+        for(let i = 0; i< string.length; i++){
+
+            let bracket = string[i]
+
+            if(dataBrackets[bracket]){ // if current string is a closing bracket check if it corresponds to last opening bracket in stack
+                let lastItem;
+                // console.log(stack)
+                // console.log(string)
+
+                if(stack.length !== 0) lastItem = stack.pop()
+
+                console.log(stack)
+                console.log(string)
+                // console.log(lastItem)
+                // console.log(dataBrackets[bracket])
+
+                if(lastItem !== dataBrackets[bracket]){ 
+
+                    // if last opening bracket in stack does not correspont to current closing bracket
+                    valid = false
+                    break
+                }
+
+            } else { 
+                stack.push(bracket) // if its is an opening bracket
+            }
+
+        }
+            valid ? result.push('YES') : result.push('NO')
+    }
+    return result
+}
+
+
+console.log(mB(['[{]}', '[{}]' ]))
+
+// console.log(mB('[{]}'))
+// console.log(mB('[({})]'))
+
+
+
+
 
 
 const isValid = (string) => {
@@ -39,8 +102,9 @@ const isValid = (string) => {
 
     return true
 }
-console.log(isValid("()"))
+// console.log(isValid('[{]}'))
 
+// console.log(isValid('[({})]'))
 
 const isValidNosubExpressions = () => {
       
