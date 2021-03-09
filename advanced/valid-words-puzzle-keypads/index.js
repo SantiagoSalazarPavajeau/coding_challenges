9b15a4fb56387bd8f3c24dcbc5483f59752877f5
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2021-03-09 13:34:29 +0100

    add js bitmask

---
+++ advanced/valid-words-puzzle-keypads/index.js
@@ -0,0 +1,38 @@
+var findNumOfValidWords = function(words, puzzles) {
+    let hm = {}
+    
+    for(let word of words){
+        let mask = 0
+        for(let i = 0; i < word.length; i++){
+            mask |= 1 << (word.charCodeAt(i) - 'a'.charCodeAt(0))
+            // add a 1 bit to position word.charCodeAt(i) from each character
+            // 
+        }
+        hm[mask] = hm[mask] ? hm[mask]+1 : 1
+    }
+    
+    let result = []
+    
+    for(let puzzle of puzzles){
+        let mask = 0
+        for(let i  = 0; i < puzzle.length; i++){
+            mask |= 1 << (puzzle.charCodeAt(i) - 'a'.charCodeAt(0))
+        }
+        let count = 0
+        let substring = mask
+        let firstChar = 1 << (puzzle.charCodeAt(0) - 'a'.charCodeAt(0))
+        
+        while(true){
+            if((substring & firstChar) === firstChar && hm[substring]){
+                count += hm[substring]
+            }
+            
+            if(substring === 0) break
+            
+            substring = (substring - 1) & mask
+        }
+        
+        result.push(count)
+    }
+    return result
+}
