1da773407ec092cbc57bfe70a207679d4b86dd4a
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2021-03-15 17:30:56 +0100

    add js sort

---
+++ longest_common_prefix/index.js
@@ -0,0 +1,15 @@
+
+const longestCommonPrefix = (strs) => {
+    
+    if(strs.length === 0) return ''
+    strs.sort() // sort alphabetically 
+    
+    let result = ''
+    
+    for(let i =0; i< strs[0].length; i++){
+        if(strs[0][i] !== strs[strs.length -1][i])
+            return result //exit when no match
+        result+=strs[0][i] // add when match
+    }
+    return result
+};
