ba37ccd47f5a2eb0630126a70532d5cacc79d800
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2021-04-19 19:01:19 -0400

    add comments

--- advanced/permutations_II/index.js
+++ advanced/permutations_II/index.js
@@ -5,16 +5,19 @@ var permuteUnique = function(nums) {
     return result
 };
 
-const permute = (result, nums, start) => {
-    
+const permute = (result, nums, start) => { 
+    // use start as pointer 
+    // modify nums directly by swap
+    // when start arrives to length of n add new permutation
     if(start === (nums.length - 1)){
-        result.push(nums)
+        result.push([...nums])
+        return
     }else{
         for(let i = start; i<nums.length ;i++){
             
             if(( i === start) || (nums[i] !== nums[start])){
                 
-                [nums[i], nums[start]] = [nums[start], nums[i]] 
+                [nums[start], nums[i]] = [nums[i], nums[start]] 
                 console.log(nums)
                 permute(result, nums, start+1)
             }
