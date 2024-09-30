0ff04fcbb96e2253a6ee409490f9245528ba755b
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2024-09-30 17:20:40 -0400

    new top k frequent elements python

--- top_k_frequent_elements/script.py
+++ top_k_frequent_elements/script.py
@@ -26,3 +26,27 @@ def top_k_frequent_elements(nums, k):
             
 
 print(top_k_frequent_elements(nums, k))
+
+from collections import Counter
+import heapq
+
+def topKFrequent(nums, k):
+    # Step 1: Count frequencies
+    count = Counter(nums)
+    # this creates a dictionary of pairs
+    # contains target number (key) and its count(value)
+
+    pdb.set_trace()
+    # Step 2: Build a heap of the k most frequent elements
+    heap = []
+
+    # count.items returns the key pairs as an array
+    for num, freq in count.items():
+        heapq.heappush(heap, (freq, num))
+        if len(heap) > k:
+            # remove the smallest frequency element
+            #keep the k largest frequency elements
+            heapq.heappop(heap)
+    
+    # Step 3: Extract elements from the heap
+    return [num for freq, num in heap]
