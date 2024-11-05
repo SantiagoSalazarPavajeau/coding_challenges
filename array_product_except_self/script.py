14c752e37ddc6383e4e842c303f4112b14693b38
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2024-11-05 13:09:27 -0500

    add steps

--- array_product_except_self/script.py
+++ array_product_except_self/script.py
@@ -5,7 +5,7 @@ import pdb
 class Solution:
 
     """
-    Contains duplicate: return true or false if array contains duplicate values
+    Product of all other items replace current
     """
     def array_product_except_self(self, nums: List[int]) -> bool:
         logging.basicConfig(level=logging.INFO)
@@ -18,7 +18,6 @@ class Solution:
             #dynamic programming
             result[i] = left_to_right
             left_to_right = left_to_right * nums[i]
-            pdb.set_trace()
 
         right_to_left = 1
         #calculated the product excluding the last element
@@ -26,8 +25,7 @@ class Solution:
             # dynamic programming
             result[i] = result[i] * right_to_left
             right_to_left = right_to_left * nums[i]
-            pdb.set_trace()
-
+        # result = [nums[3] * nums[2] * nums[1], nums[0] * nums[3] * nums[2], nums[0] * nums[1] * nums[3], nums[0] * nums[1] * nums[2]], suffix = nums[3] * nums[2] * nums[1] * nums[0]
         logging.info("Result is : {}".format(result))
         return result
     
