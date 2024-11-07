1d48ecfbe524fa0a81c83c17982265b6c2544b9c
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2024-11-07 16:58:32 -0500

    simplify

--- minimum-in-rotated-subarray/script.py
+++ minimum-in-rotated-subarray/script.py
@@ -12,30 +12,18 @@ class Solution:
         result = nums[0]
         left = 0
         right = len(nums) -1
-        while left <= right:
-            # we try to find the smallest number so far
-            # between the number at 0 index, the number in current left pointer and the number in right pointer.
-            # we will always compare the lowest number we have found to the current number we are looking at
-            if nums[left] < nums[right]:
-                result = min(result, nums[left])
-                break
-            
-            # binary search by breaking array in half
-            middle = (left + right) // 2
+        while left < right:
+            # no remainder divide in half
+            mid = (left + right ) // 2
 
-            # now we have a new number at middle 
-            # so we can also see if that one is the lowest we have seen
-            result = min(result, nums[middle])
-
-            # the binary search finds reassigns left and right pointer to new middle
-            # based on which side has a lower number
-            # this works because array is sorted
-            if nums[middle] >= nums[left]:
-                left = middle + 1
+            if nums[mid] > nums[right]:
+                #smallest value is in right 
+                left = mid+1
             else:
-                right = middle - 1
+                right = mid
 
         logging.info("Result is : {}".format(result))
+        result = nums[left]
         return result
 
 
